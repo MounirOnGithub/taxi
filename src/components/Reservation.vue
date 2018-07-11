11066badcf40307b744fb333399110e24362528d
Mounir Khanouri <m.khanouri@live.fr>
2018-07-11 23:31:15 +0200

    First commit

---
+++ src/components/Reservation.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="reservation">
+        <input v-model="name" type="text">
+        <input v-model="phone" type="text">
+        <input v-model="departure" type="text">
+        <input v-model="destination" type="text">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Reservation',
+  data () {
+    return {
+      name: '',
+      phone: '',
+      departure: '',
+      destination: '',
+      reservationDate: null,
+      creationDate: null
+    }
+  }
+}
+</script>
