a0777b3d941dc6ccaa58dc16311afc1039b730f3
Mounir Khanouri <mounir.khanouri@sogeti.com>
2018-07-12 17:08:20 +0200

    feat: validation form and displaying errors

--- src/forms/ReservationForm.vue
+++ src/forms/ReservationForm.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="container">
       <div class="row">
+        <p v-if="formErrors">
+          The form is not valid, you should correct this errors for submitting :
+          <ul v-if="formErrors.length">
+            <li v-for="error in formErrors">
+              {{ error }}
+            </li>
+          </ul>
+        </p>
         <div class="col-md-6">
           <input v-model="name" type="text" placeholder="Name">
           <input v-model="phone" type="text" placeholder="Phone">
@@ -17,6 +25,9 @@
 <script>
 import datePicker from 'vue-bootstrap-datetimepicker'
 import 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css'
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const phoneRegex = /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/
 
 export default {
   name: 'ReservationForm',
@@ -35,14 +46,50 @@ export default {
       config: {
         format: 'DD/MM/YYYY HH:mm',
         useCurrent: false
-      }
+      },
+      formErrors: []
     }
   },
   methods: {
     submitReservation: function () {
-      this.validateForm()
+      isFormValid = this.validateForm()
+      if (!isFormValid) {
+        console.log('The form is not valid')
+        return
+      }
     },
-    validateForm: function () {}
+    validateForm: function () {
+      // Is name !empty & under 30 characters
+      if (this.name === '' || this.name.length > 30) {
+        this.formErrors.push('Name should be under 30 characters and not empty')
+        return false
+      }
+      // Is phone field a phone number
+      if (this.phone === '') {
+        if (!phoneRegex.test(this.phone)) {
+          this.formErrors.push('Phone number is not valid')
+          return false
+        }
+      }
+      // Is departure found in Google Maps API
+      if (this.departure === '') {
+        this.formErrors.push('Departure not found')
+        return false
+      }
+      // Is destination found in Google Maps API
+      if (this.destination === '') {
+        this.formErrors.push('Destination not found')
+        return false
+      }
+      // Is the date of reservation is in the past     
+      let now = new Date();
+      now.setHours(0,0,0,0);
+      if (this.date < now) {
+        this.formErrors.push('The reservation date should not be in the past')
+        return false
+      }
+      return true
+    }
   }
 }
 </script>
